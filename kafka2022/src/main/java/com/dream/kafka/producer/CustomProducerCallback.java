8a25c04aca4ae1c54eb80ef2384627142bab7618
huzejun <huzejun1990>
2022-04-15 21:08:24 +0800

    kafka 生产者 回调 异步发送数据 2022-4-15 21:08:14

---
+++ kafka2022/src/main/java/com/dream/kafka/producer/CustomProducerCallback.java
@@ -0,0 +1,48 @@
+package com.dream.kafka.producer;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * @Author : huzejun
+ * @Date: 2022/4/15-20:01
+ */
+public class CustomProducerCallback {
+
+    public static void main(String[] args) {
+
+        // 0 配置
+       Properties properties = new Properties();
+
+       //连接集群
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"master:9092,slave1:9092");
+
+        //指定对应的序列化类型
+//        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //1、创建kafka生产者对象
+        // "" hello
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+
+        //2、发送数据
+        for (int i = 0; i < 5; i++) {
+            kafkaProducer.send(new ProducerRecord<>("first", "dream" + i), new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+
+                    if (e == null) {
+                        System.out.println("主题： "+recordMetadata.topic() + "分区： "+ recordMetadata.partition());
+                    }
+                    
+                }
+            });
+        }
+
+        //3、关闭资源
+        kafkaProducer.close();
+    }
+}
