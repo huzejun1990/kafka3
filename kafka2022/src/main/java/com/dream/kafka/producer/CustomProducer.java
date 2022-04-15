0196e090ec7de6386ed68ecb790a0529541ad700
huzejun <huzejun1990>
2022-04-15 20:48:23 +0800

    构造项目 + kafka 异步发送数据 2022-4-15 20:48:08

---
+++ kafka2022/src/main/java/com/dream/kafka/producer/CustomProducer.java
@@ -0,0 +1,42 @@
+package com.dream.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * @Author : huzejun
+ * @Date: 2022/4/15-20:01
+ */
+public class CustomProducer {
+
+    public static void main(String[] args) {
+
+        // 0 配置
+       Properties properties = new Properties();
+
+       //连接集群
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"master:9092,slave1:9092");
+
+        //指定对应的序列化类型
+//        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //1、创建kafka生产者对象
+        // "" hello
+        //2022年4月15日20:56:22
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+
+        //2、发送数据
+        for (int i = 0; i < 5; i++) {
+            kafkaProducer.send(new ProducerRecord<>("first","dream" + i));
+        }
+
+        //3、关闭资源
+        kafkaProducer.close();
+    }
+}
